72538d2a3160d7363802dcb8d3a429455d92dc83
황현규 <zmqpg1120@naver.com>
2025-12-07 16:21:10 +0900

    Update gacha_machine.py

--- widgets/gacha_machine.py
+++ widgets/gacha_machine.py
@@ -1,5 +1,5 @@
-# <Gacha-Widget> - Gacha Machine Desktop App
-# Copyright (C) <2025> <Hyeongyu Hwang>
+# This file is part of <Gacha-Widget>.
+# Copyright (C) 2025 <Hyeongyu Hwang>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -8,11 +8,11 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program. If not, see [https://www.gnu.org/licenses/](https://www.gnu.org/licenses/).
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import random
 
@@ -238,3 +238,4 @@ class MainGachaWidget(QWidget):
             widget.close() 
 
         self.spawned_widgets.clear()
+
