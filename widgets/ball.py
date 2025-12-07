c1e740f77d1344a35e8806b344ef910df5f1219c
황현규 <zmqpg1120@naver.com>
2025-12-07 16:11:19 +0900

    license

--- widgets/ball.py
+++ widgets/ball.py
@@ -1,3 +1,19 @@
+# <Gacha-Widget> - Gacha Machine Desktop App
+# Copyright (C) <2025> <Hyeongyu Hwang>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see [https://www.gnu.org/licenses/](https://www.gnu.org/licenses/).
+
 from PyQt6.QtWidgets import QWidget, QLabel
 from PyQt6.QtGui import QMouseEvent, QPixmap
 from PyQt6.QtCore import Qt, QPoint
@@ -138,4 +154,5 @@ class BallWidget(QWidget):
 
     def create_trace(self, position):
         trace = TraceWidget(self, position)
+
         self.traces.append(trace)
